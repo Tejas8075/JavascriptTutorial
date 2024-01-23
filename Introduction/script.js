ab2fc21c8d345838b519f41fa1b17772f8d9c531
Tejas8075 <tejasmahangade61@gmail.com>
2024-01-23 12:10:46 +0530

    Add new code

--- Introduction/script.js
+++ Introduction/script.js
@@ -1,2 +1,13 @@
 console.log("Hello World");
+console.log("hey ");
 alert("Hello World");
+let a = prompt ("Enter your number")
+console.log("Your number is " +a);
+let isTrue = confirm("Are you sure you want to leave this page?");
+if(isTrue){
+    console.log("Computer blawsts");
+}
+else{
+    console.log("Computer does not blawsts");
+}
+
